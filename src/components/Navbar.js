2e75ff5de96df5777c69ec1859246dcacca5f497
Divyanshu Baghel <divyanshubaghel1@gmail.com>
2023-01-20 15:56:38 +0530

    ad

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
   return (
         <nav className="navbar navbar-expand-lg bg-dark " data-bs-theme="dark">
             <div className="container-fluid">
-                <div style={{display:"flex"}}><Link className="navbar-brand d-flex" to="/"><h1 className='text-primary d-flex'>Qy</h1>_Music</Link></div>
+                <div style={{display:"flex"}}><Link className="navbar-brand d-flex" to="/"><p className='text-primary d-flex'>Qy</p>_Music</Link></div>
                 {/*<button className="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
